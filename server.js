501356fed183f8e7f9aaf3542efcce46249add48
Mitchell Romney <mitchell@theearthissquare.com>
2019-12-30 01:22:56 +1100

    feat(app): add ssl redirect force

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+const sslRedirect = require('heroku-ssl-redirect');
 
-let app = express();
-app.use(history());
-app.use(serveStatic(__dirname + "/dist"));
+let app = express()
 
-let port = process.env.PORT || 5000;
-app.listen(port);
+app.use(history())
+app.use(serveStatic(__dirname + "/dist"))
+app.use(sslRedirect())
+
+let port = process.env.PORT || 5000
+app.listen(port)
